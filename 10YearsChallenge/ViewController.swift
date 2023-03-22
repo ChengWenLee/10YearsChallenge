14fa34aaaaad76355691852e3c31e87af046c814
ChengWenLee <ChanWenLee@gmail.com>
2023-03-22 15:39:58 +0800

    Initial Commit

---
+++ 10YearsChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10YearsChallenge
+//
+//  Created by Lee chanwen on 3/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
